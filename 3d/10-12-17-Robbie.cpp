c23cf650a683ff4f1c4243dba6ed3cbd2b137200
Robbie Selwyn <robbie@selwynsoftware.com>
2017-10-12 14:11:24 -0700

    Added Thursday code

--- 3d/10-12-17-Robbie.cpp
+++ 3d/10-12-17-Robbie.cpp
@@ -1,9 +1,10 @@
 //Declare any variables shared between functions here
 
-enum ROBOT_STATE {MOVING_TO_DRILL, STARTING_DRILL, DRILLING, DROPPING_OFF};
+enum ROBOT_STATE {MOVING_TO_DRILL, STARTING_DRILL, AVOIDING_CAMPERS, DRILLING, DROPPING_OFF};
 
 #define DISTANCE_FROM_CENTER 0.12f
-#define CLOSEST_TO_WALL .530f
+#define CLOSEST_TO_WALL .535f
+#define DROPOFF_TIMEOUT 45
 
 ROBOT_STATE state;
 
@@ -24,32 +25,35 @@ bool evadedGeyser;
 
 float spinAlignQuat[4];
 
+int dropOffTimer;
+
 void init(){
+    dropOffTimer = DROPOFF_TIMEOUT;
     api.getMyZRState(myState);
     
     color = (myState[0] > 0); // TRUE FOR BLUE, FALSE FOR RED
 
     // api.setPosGains(1,0,5);
-    //This function is called once when your code is first loaded.
+	//This function is called once when your code is first loaded.
 
-    //IMPORTANT: make sure to set any variables that need an initial value.
-    //Do not assume variables will be set to 0 automatically!
-    state = MOVING_TO_DRILL;
-    hasPickedUp = false;
-    movementTarget[0] = .2 * (color ? 1 : -1);
-    movementTarget[1] = .2 * (color ? 1 : -1);
-    movementTarget[2] = .515 ;
-    
-    for (int i = 0; i <3; i++) zero[i] = 0;
-//  zero[2] += .10;
-    farDisMat[0] = 0;
-    farDisMat[1] = 0;
-    farDisMat[2] = -0.5;
+	//IMPORTANT: make sure to set any variables that need an initial value.
+	//Do not assume variables will be set to 0 automatically!
+	state = MOVING_TO_DRILL;
+	hasPickedUp = false;
+	movementTarget[0] = .2 * (color ? 1 : -1);
+	movementTarget[1] = .2 * (color ? 1 : -1);
+	movementTarget[2] = .525 ;
+	
+	for (int i = 0; i <3; i++) zero[i] = 0;
+// 	zero[2] += .10;
+	farDisMat[0] = 0;
+	farDisMat[1] = 0;
+	farDisMat[2] = -0.5;
 
     attTarget[0] = 1;
-    attTarget[1] = 0;
-    attTarget[2] = 0;
-    
+	attTarget[1] = 0;
+	attTarget[2] = 0;
+	
     drillTarget[0] = 0;
     drillTarget[1] = 0;
     drillTarget[2] = 1;
@@ -72,6 +76,7 @@ void flattenMe() {
 }
 
 void loop(){
+    DEBUG(("THURSDAY SUBMISSION"));
     api.getMyZRState(myState);
     velocity = sqrtf(myState[3] * myState[3]  + myState[4] * myState[4] + myState[5] * myState[5]);
     float rotV = sqrtf(myState[9] * myState[9]  + myState[10] * myState[10] + myState[11] * myState[11]);
@@ -83,7 +88,9 @@ void loop(){
     myPos[1] = myState[1];
     myPos[2] = myState[2];
     // game.pos2square(myPos, mySquare);
+    
     bool geyserOnMe = game.isGeyserHere(myState);
+    DEBUG(("GEYSER ON ME? %d", geyserOnMe));
     
 
     if (state == MOVING_TO_DRILL) {
@@ -94,9 +101,12 @@ void loop(){
             flattenMe();
         // }
         
-        // DEBUG(("%f", distance));
-        
-        if (distance < .07 && velocity < .0055 && myState[2] < CLOSEST_TO_WALL) {
+        DEBUG(("%f < Z DIST", (fabsf(myState[2] - movementTarget[2]))));
+        // checks: Overall distance < .07. Velocity < .055, we are above the target, we are closer than .10 in the z direction
+        // if (distance < .07 && velocity < .0055 && myState[2] < CLOSEST_TO_WALL && fabsf(myState[2] - movementTarget[2]) < .015) {
+        // NEW VERSION
+         // checks: Overall distance < .07. Velocity < .0055, we are between .01 and .04 m above the drilling floor
+        if (distance < .2 && velocity < .0055 && CLOSEST_TO_WALL - myState[2] > .01 && CLOSEST_TO_WALL - myState[2] < .04) {
             state = STARTING_DRILL;
         }
         
@@ -132,17 +142,44 @@ void loop(){
              
             if (geyserOnMe) {
                 DEBUG(("GEYSER ON ME EVADING"));
-                zero[2] = -.10;
-                evadedGeyser = true;
+            	zero[2] = -.11;
+            	evadedGeyser = true;
             }
             else {
-                zero[2] = .10;
+            	zero[2] = .11;
             }
 
         }
     }
     
+    if (state == AVOIDING_CAMPERS) {
+        state = MOVING_TO_DRILL;
+        movementTarget[0] -= 0.08 * (color ? 1 : -1); // move our drill location
+
+        game.dropSample(0);
+        game.dropSample(1);
+    }
+    
+    DEBUG(("%d << DROPOFF", dropOffTimer));
     if (state == DROPPING_OFF) {
+        DEBUG(("DROPPING OFF"));
+        dropOffTimer -= 1;
+        if (geyserOnMe) {
+            evadedGeyser = true;
+            zero[2] = -.10; 
+            DEBUG(("REROUTING FOR GEYSER"));
+        }
+        
+        // if (dropOffTimer == 0) {
+        //     DEBUG(("It seems you are camping at the base station."));
+        //     DEBUG(("It seems you are camping at the base station."));
+        //     DEBUG(("It seems you are camping at the base station."));
+        //     DEBUG(("It seems you are camping at the base station."));
+            
+        //     state = AVOIDING_CAMPERS;
+        //     dropOffTimer = DROPOFF_TIMEOUT;
+        // }
+        
         api.setPositionTarget(zero);
         api.setAttitudeTarget(farDisMat);
         
@@ -150,7 +187,8 @@ void loop(){
             game.dropSample(0);
             game.dropSample(1);
             game.dropSample(2);
-            if (game.getFuelRemaining() > .17) {
+            dropOffTimer = DROPOFF_TIMEOUT;
+            if (game.getFuelRemaining() > .13) {
                 state = MOVING_TO_DRILL;
             }
             movementTarget[0] -= 0.08 * (color ? 1 : -1); // move our drill location
@@ -158,7 +196,8 @@ void loop(){
                 movementTarget[2] = .510;
             }
             else {
-                movementTarget[2] = .486; // hacky way to reduce overshooting
+                DEBUG(("CHANGING TARGET"));
+                movementTarget[2] = .495; // hacky way to reduce overshooting
             }
             // movementTarget[1] -= 0.08 * (color ? 1 : -1);
 
